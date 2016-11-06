73b327d65f171a875ac8eaad923c0b5ee1cd98ca
Anthony Dinh <pookieofdoom@gmail.com>
2016-11-05 17:05:03 -0700

    PLEASE PATRICK

---
+++ app/src/main/java/adinh03/calpoly/edu/cpbadq/WaitingQueue.java
@@ -0,0 +1,13 @@
+package adinh03.calpoly.edu.cpbadq;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Anthony on 11/5/16.
+ */
+
+public class WaitingQueue {
+   private ArrayList<User> mQueue;
+
+
+}
