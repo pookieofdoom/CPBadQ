468abe3544f9fef3b1536d9c84479e481a7284ce
DerekNguyen <derek.nguyen.t@gmail.com>
2016-11-05 18:41:10 -0700

    Merge branch 'master' of /Users/LinkedList/AndroidStudioProjects/CPBadQ with conflicts.

--- app/src/main/java/adinh03/calpoly/edu/cpbadq/CourtActivity.java
+++ app/src/main/java/adinh03/calpoly/edu/cpbadq/CourtActivity.java
@@ -3,9 +3,9 @@ package adinh03.calpoly.edu.cpbadq;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Anthony on 11/5/16.
@@ -13,25 +13,18 @@ import android.widget.TextView;
 
 public class CourtActivity extends AppCompatActivity
 {
-   private TextView mOnCourt;
-   private RecyclerView mRecylerView;
-   private MyAdapter adapter;
-   private WaitingQueue mQueue;
 
+   private int numWaiting = 0;
+   //private ArrayList<User> onCourt;
+   //private ArrayList<User> waitingList;
+   private RecyclerView waitingList;
    @Override
    protected void onCreate(@Nullable Bundle savedInstanceState)
    {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_court);
-      mOnCourt = (TextView) findViewById(R.id.onCourtList);
-      mRecylerView = (RecyclerView) findViewById(R.id.waitingList);
-      mRecylerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-      adapter = new MyAdapter(mQueue);
-
-
-
-
    }
+
 
 
 
