7c729b60f8b0d310b14850172e4ee827949b9f6e
ggyboys <pmcdonnal@gmail.com>
2016-11-05 17:56:28 -0700

    What do i even do here?

--- app/src/main/java/adinh03/calpoly/edu/cpbadq/LogInActivity.java
+++ app/src/main/java/adinh03/calpoly/edu/cpbadq/LogInActivity.java
@@ -23,6 +23,7 @@ public class LogInActivity extends AppCompatActivity {
    @Override
    protected void onCreate(@Nullable Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_log_in);
 
       // Initialize FirebaseAuth
       mFirebaseAuth = FirebaseAuth.getInstance();
--- app/src/main/java/adinh03/calpoly/edu/cpbadq/SignUpActivity.java
+++ app/src/main/java/adinh03/calpoly/edu/cpbadq/SignUpActivity.java
@@ -2,41 +2,68 @@ package adinh03.calpoly.edu.cpbadq;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 
 public class SignUpActivity extends AppCompatActivity {
     Button signupButton;
-    EditText Email;
-    EditText Password;
+    EditText email;
+    EditText password;
 
     private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
 
         // Initialize FirebaseAuth
         mFirebaseAuth = FirebaseAuth.getInstance();
 
         signupButton = (Button) findViewById(R.id.signupButton);
 
-        Email = (EditText) findViewById(R.id.email);
-        Password = (EditText) findViewById(R.id.password);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
 
 
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(SignUpActivity.this, LogInActivity.class);
-                startActivity(i);
+                String getEmail = email.getText().toString();
+                String getPassword = password.getText().toString();
+                getEmail = getEmail.trim();
+                getPassword = getPassword.trim();
+                if (getEmail.isEmpty() || getPassword.isEmpty() || getEmail == null || getPassword == null) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage("Please enter a valid email and password")
+                            .setTitle("Error")
+                            .setPositiveButton("Okay", null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    mFirebaseAuth.createUserWithEmailAndPassword(getEmail, getPassword)
+                            .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    Intent i = new Intent(SignUpActivity.this, LogInActivity.class);
+                                    startActivity(i);
+                                }
+                            });
+                }
+
             }
         });
+
     }
 }
